ca68f78ea18415560efbf1cae120cf5bd5315735
Joao Felipe Santos <joao.eel@gmail.com>
2011-07-28 13:53:15 -0300

    Iniciada implementação do storage para quotes usando uma base de dados SQLite. Essa base será acessada a partir da classe QuoteProvider.

--- src/com/seaandsailor/citadroid/CitaDroid.java
+++ src/com/seaandsailor/citadroid/CitaDroid.java
@@ -1,9 +1,13 @@
 package com.seaandsailor.citadroid;
 
 import java.io.InputStream;
+
 import com.seaandsailor.citadroid.R;
+import com.seaandsailor.citadroid.Quote.Quotes;
 
 import android.app.Activity;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -12,23 +16,42 @@ import android.widget.TextView;
 
 public class CitaDroid extends Activity {
 	/** Called when the activity is first created. */
+	
+	private static final String[] PROJECTION = new String[] {
+        Quotes._ID, // 0
+        Quotes.QUOTE, // 1
+        Quotes.AUTHOR, // 2
+	};
 	private Button generate;
 	private TextView text;
-	private QuoteDb quotes;
+	private QuoteProvider quotes;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 		InputStream txt = getResources().openRawResource(R.raw.batema);
-		quotes = new QuoteDb(txt);
+		quotes = new QuoteProvider();
 		generate = (Button) this.findViewById(R.id.button);
 		text = (TextView) this.findViewById(R.id.text);
-		text.setText(quotes.getQuote());
+		String[] quote = getQuote();
+		text.setText(quote[0] + ": " + quote[1]);
 		this.generate.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
-				text.setText(quotes.getQuote());
+				String[] quote = getQuote();
+				text.setText(quote[0] + ": " + quote[1]);
 			}
 		});
 	}
+	
+	/**
+	 * Retorna um array de strings onde o índice 0 indica o autor e o índice 1 a citação.
+	 * @return
+	 */
+	private String[] getQuote() {
+		Cursor q = managedQuery(Uri.parse("quotes"), PROJECTION, "", null, "");
+		String[] res = {q.getString(1), q.getString(2)};
+		return res;
+	}
+	
 }
