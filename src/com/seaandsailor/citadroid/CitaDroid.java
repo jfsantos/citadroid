41675ffb5eb9ea4081583e393f5fbea26b94456d
Joao Felipe Santos <joao.eel@gmail.com>
2011-08-05 13:37:03 -0300

    Adicionado botão de compartilhamento. O compartilhamento é feito através de um Intent ACTION_SEND de texto puro, e já foi testado com os aplicativos do GMail e do Twitter.

--- src/com/seaandsailor/citadroid/CitaDroid.java
+++ src/com/seaandsailor/citadroid/CitaDroid.java
@@ -4,6 +4,7 @@ import java.io.InputStream;
 import com.seaandsailor.citadroid.R;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -12,7 +13,7 @@ import android.widget.TextView;
 
 public class CitaDroid extends Activity {
 	/** Called when the activity is first created. */
-	private Button generate;
+	private Button generate, share;
 	private TextView text;
 	private QuoteDb quotes;
 
@@ -22,12 +23,23 @@ public class CitaDroid extends Activity {
 		setContentView(R.layout.main);
 		InputStream txt = getResources().openRawResource(R.raw.batema);
 		quotes = new QuoteDb(txt);
-		generate = (Button) this.findViewById(R.id.button);
+		generate = (Button) this.findViewById(R.id.generatebutton);
+		share = (Button) this.findViewById(R.id.sharebutton);
 		text = (TextView) this.findViewById(R.id.text);
 		text.setText(quotes.getQuote());
 		this.generate.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
 				text.setText(quotes.getQuote());
+			}
+		});
+		this.share.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				Intent sharingIntent = new Intent(Intent.ACTION_SEND);
+				String txt = text.getText().toString();
+
+				sharingIntent.setType("text/plain");
+				sharingIntent.putExtra(Intent.EXTRA_TEXT, txt);
+				startActivity(Intent.createChooser(sharingIntent, "Compartilhar"));
 			}
 		});
 	}
